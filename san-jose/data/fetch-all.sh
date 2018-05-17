754ae079bbe26e321397abb015fa00b2e51f436d
Steven Buss <steven.buss@gmail.com>
2018-05-16 21:40:52 -0700

    Fix the broken lines

--- san-jose/data/fetch-all.sh
+++ san-jose/data/fetch-all.sh
@@ -3,3 +3,4 @@ for i in {2001..2017}; do
     wget https://sjpermits.org/permits/ftproot/SanJose/permitdataYears/PD_${i}_FINAL.TXT
     wget https://sjpermits.org/permits/ftproot/SanJose/permitdataYears/PD_${i}_ISSUE.TXT
 done
+cd ../../ && patch -p0 < san-jose/data/fix-dates.patch
